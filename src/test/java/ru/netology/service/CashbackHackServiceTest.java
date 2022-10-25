86a4a6c974a5317c12ae17d7a9171c5a7e1431c2
Artem <ptv10378@gmail.com>
2022-10-25 22:27:11 +0300

    add service and tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void remain1() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(expected, actual);
+        ;
+    }
+
+    @Test
+    public void remain2() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1800);
+        int expected = 200;
+        assertEquals(expected, actual);
+        ;
+    }
+
+    @Test
+    public void remain3() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(expected, actual);
+        ;
+    }
+
+    @Test
+    public void remain4() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected, actual);
+        ;
+    }
+}
